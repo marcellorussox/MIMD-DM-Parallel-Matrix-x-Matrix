253928b59c3f8288b94e9a581ac5b4f7d6ea0de8
rafdan28 <rafdanna28@gmail.com>
2023-12-27 10:36:50 +0100

    aggiunti nomi dei parametri per la documentazione

--- headers/data_structures.h
+++ headers/data_structures.h
@@ -3,16 +3,17 @@
 
 #define D_TAG 22
 
-void print_matrix(double*, int);
-void create_grid(MPI_Comm*, MPI_Comm*, MPI_Comm*,int, int, int, int*, int, int*);
-void send_matrix_from_processor_0(double*, int, int, MPI_Comm, int);
-void receive_matrix_from_processor_0(double*, int, int);
-void merge(double*, int, double*, int, MPI_Comm, int, int);
-void multiply(double*, double*, double*, int);
-void bmr(double*, double*, double*, int, MPI_Comm, MPI_Comm, MPI_Comm, int, int[2]);
+void print_matrix(double* matrix, int matrix_order);
+void create_grid(MPI_Comm* grid, MPI_Comm* sub_rgrid, MPI_Comm* sub_cgrid, int mpi_rank, int mpi_size, int grid_order, int* periods, int reorder, int* coords);
+void send_matrix_from_processor_0(double* matrix, int matrix_order, int sub_matrix_order, MPI_Comm grid, int mpi_size);
+void receive_matrix_from_processor_0(double* sub_matrix, int sub_matrix_order, int mpi_rank);
+void merge(double* matrix_c, int matrix_order, double* sub_matrix_c, int sub_matrix_order, MPI_Comm grid, int mpi_size, int mpi_rank);
+void multiply(double* matrix_a, double* matrix_b, double* matrix_c, int matrix_order);
+void bmr(ouble* sub_matrix_a, double* sub_matrix_b, double* sub_matrix_c, int sub_matrix_order,
+         MPI_Comm grid, MPI_Comm sub_rgrid, MPI_Comm sub_cgrid, int grid_order, int coords[2]);
 
 
-double* initialize_matrix(int);
-double* generate_random_matrix(int);
+double* initialize_matrix(int matrix_order);
+double* generate_random_matrix(int matrix_order);
 
 #endif
